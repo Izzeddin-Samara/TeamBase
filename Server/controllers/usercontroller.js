e2e4d30edf35b3b87d9b19b9bb4644b3702fc4cd
Izzsamara <izzidinsamara@gmail.com>
2025-05-01 14:01:51 +0300

    Add controllers directory with user controller

---
+++ Server/controllers/usercontroller.js
@@ -0,0 +1,48 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+    // Register Function
+    register: (req, res) => {
+        const { firstName, lastName, email, password, confirmPassword } = req.body;
+
+        
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password
+        });
+
+        newUser.confirmPassword = confirmPassword; 
+
+        newUser.save()
+            .then(user => res.json({ msg: "Success!", user }))
+            .catch(err => res.status(400).json(err));
+    },
+
+    // Login Function
+    login: async (req, res) => {
+        try {
+            const user = await User.findOne({ email: req.body.email });
+            if (!user) return res.status(400).json({ error: "Email not found" });
+
+            const correctPassword = await bcrypt.compare(req.body.password, user.password);
+            if (!correctPassword) return res.status(400).json({ error: "Invalid password" });
+
+            const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, { expiresIn: '1h' });
+
+            res.cookie("usertoken", token, { httpOnly: true })
+                .json({ msg: "success!" });
+        } catch (err) {
+            res.status(500).json({ error: "Something went wrong", details: err.message });
+        }
+    },
+    logout: (req, res) => {
+        res.clearCookie("usertoken");
+        res.json({ msg: "Logged out successfully" });
+    }
+
+};
+
