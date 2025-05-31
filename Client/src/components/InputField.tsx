a1a67baccebf032b6bdaa0d90de831357a9e7a1b
Izzsamara <izzidinsamara@gmail.com>
2025-05-31 14:56:59 +0300

    Add error displaying message on InputField component

--- Client/src/components/InputField.tsx
+++ Client/src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 interface InputProps {
   type: string;
@@ -6,19 +6,33 @@ interface InputProps {
   value: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
   name: string;
+  error?: string;
 }
 
-const InputField: React.FC<InputProps> = ({ type, placeholder, value, onChange, name}) => {
+const InputField: React.FC<InputProps> = ({
+  type,
+  placeholder,
+  value,
+  onChange,
+  name,
+  error,
+}) => {
   return (
-    <input
-      type={type}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      name={name}
-      className="w-full border border-gray-300 outline-none focus:ring-2 text-xl p-4 rounded-lg focus:ring-blue-800 mt-4"
-
-    />
+    <div className="w-full flex flex-col">
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        name={name}
+        className={`w-full border border-gray-300 outline-none focus:ring-3 text-sm md:text-lg md:p-4 p-4 rounded-lg focus:ring-blue-800 mt-4 ${
+          error
+            ? "border-red-600 focus:ring-red-600"
+            : "border-gray-300 focus:ring-blue-800"
+        }`}
+      />
+      {error && <span className="text-red-600 text-sm mt-1">{error}</span>}
+    </div>
   );
 };
 
