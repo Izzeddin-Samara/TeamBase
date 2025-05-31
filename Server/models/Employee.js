ac2fcc9af892a8adf45803198d7baccbb975cb69
Izzsamara <izzidinsamara@gmail.com>
2025-05-31 14:45:58 +0300

    Remove profilePicture and workLocation, and remove 'required: true' from hireDate, salary, and manager on Employee schema

--- Server/models/Employee.js
+++ Server/models/Employee.js
@@ -1,19 +1,20 @@
 const mongoose = require("mongoose");
 
-const employeeSchema = new mongoose.Schema({
-  employeeId: { type: String, required: true, unique: true },
-  fullName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  phoneNumber: { type: String, required: true },
-  jobTitle: { type: String, required: true },
-  department: { type: String, required: true },
-  hireDate: { type: Date, required: true },
-  salary: { type: Number, required: true },
-  workLocation: { type: String, required: true },
-  manager: { type: String, required: true },
-  profilePicture: { type: String, required: true },
-  notes: { type: String },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User",required: true},
-}, { timestamps: true });
+const employeeSchema = new mongoose.Schema(
+  {
+    employeeId: { type: String, required: true, unique: true },
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phoneNumber: { type: String, required: true, unique: true },
+    jobTitle: { type: String, required: true },
+    department: { type: String, required: true },
+    hireDate: { type: Date }, 
+    salary: { type: Number },
+    manager: { type: String },
+    notes: { type: String }, 
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Employee", employeeSchema);
