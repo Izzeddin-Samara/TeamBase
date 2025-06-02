c7733a52dc3c74180aaee43dac0d66884a01f5b3
Izzsamara <izzidinsamara@gmail.com>
2025-06-02 03:18:09 +0300

    Add console.log for register function

--- Server/controllers/usercontroller.js
+++ Server/controllers/usercontroller.js
@@ -20,6 +20,7 @@ module.exports = {
       .save()
       .then((user) => res.json({ msg: "Success!", user }))
       .catch((err) => res.status(400).json(err));
+      console.log(err)
   },
 
   // Login Function
