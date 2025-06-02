8168f8c84cb3a1ba5c35c43aa9f19b743094b742
Izzsamara <izzidinsamara@gmail.com>
2025-06-02 14:49:40 +0300

    Add Footer.tsx component

---
+++ Client/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer: React.FC = () => {
+  return (
+    <>
+      <footer className="mt-20 text-center text-gray-600 text-sm">
+        <p>© 2025 TeamBase. All rights reserved.</p>
+      </footer>
+    </>
+  );
+};
+export default Footer;
