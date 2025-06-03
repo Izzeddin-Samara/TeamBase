6fac2816afd2ea90979548589f5173c84680bd80
Izzsamara <izzidinsamara@gmail.com>
2025-06-03 23:10:55 +0300

    Remove MarginTop from Footer

--- Client/src/components/Footer.tsx
+++ Client/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer: React.FC = () => {
   return (
     <>
-      <footer className="mt-20 text-center text-gray-600 text-sm">
+      <footer className="text-center text-gray-600 text-sm">
         <p>© 2025 TeamBase. All rights reserved.</p>
       </footer>
     </>
