ead471bb986c04df65b29e078b3f4db8d10f1323
Izzsamara <izzidinsamara@gmail.com>
2025-06-05 18:03:59 +0300

    Add auth.js middleware and used in employeesroutes

--- Server/routes/employeeroutes.js
+++ Server/routes/employeeroutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const EmployeeController = require('../controllers/employeecontroller');
 const router = express.Router();
+const auth = require("../middleware/auth");
 
-router.post('/', EmployeeController.addEmployee);
-router.patch('/:id', EmployeeController.updateEmployee);
-router.get('/:id', EmployeeController.getEmployee);
-router.get('/', EmployeeController.getAllEmployees);
-router.delete('/:id', EmployeeController.deleteEmployee);
+
+router.post('/', auth, EmployeeController.addEmployee);
+router.patch('/:id', auth, EmployeeController.updateEmployee);
+router.get('/:id', auth, EmployeeController.getEmployee);
+router.get('/', auth, EmployeeController.getAllEmployees);
+router.delete('/:id', auth, EmployeeController.deleteEmployee);
 
 module.exports = router;
