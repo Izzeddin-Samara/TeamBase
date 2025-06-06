b6b7e00d6a4224857d789edfd519e0a4cf99ddb1
Izzsamara <izzidinsamara@gmail.com>
2025-06-06 19:00:52 +0300

    Add Mission component and use it in AboutPage component

--- Client/src/components/about/AboutPage.tsx
+++ Client/src/components/about/AboutPage.tsx
@@ -1,11 +1,15 @@
 import Navbar from "../Navbar";
 import Aboutus from "./Aboutus";
+import Mission from "./Mission";
 
 export default function AboutPage() {
   return (
     <>
       <Navbar />
-      <Aboutus/>
+      <div className="max-w-7xl mx-auto py-30 ">
+        <Aboutus />
+        <Mission />
+      </div>
     </>
   );
 }
---
+++ Client/src/components/about/Mission.tsx
@@ -0,0 +1,21 @@
+export default function Mission() {
+  return (
+    <>
+      <section className="py-30">
+        <h1 className="text-center text-4xl text-blue-800 font-bold py-">
+          Our Mission
+        </h1>
+        <p className="mt-8 text-md md:text-xl p-4 text-justify text-center">
+          TeamBase is a powerful yet intuitive platform that helps teams stay
+          organized, aligned, and efficient. Whether you're managing a small
+          startup or coordinating remote teams, TeamBase brings structure to
+          your team’s daily workflow — all in one place. It was born from a
+          simple idea: teams work better when everyone understands their roles,
+          responsibilities, and collaborations. TeamBase solves the real pain of
+          unclear communication and chaotic team management by giving you
+          clarity and control.
+        </p>
+      </section>
+    </>
+  );
+}
