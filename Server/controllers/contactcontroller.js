6545f198c0886d8ad54d1d54fe3d18830b27cd01
Izzsamara <izzidinsamara@gmail.com>
2025-06-07 14:46:36 +0300

    Apply node-mailjet in contactcontroller to send the user a confirm email message

--- Server/controllers/contactcontroller.js
+++ Server/controllers/contactcontroller.js
@@ -1,6 +1,39 @@
 const Contact = require("../models/Contact");
+const Mailjet = require("node-mailjet");
+require('dotenv').config();
 
+const mailjet = Mailjet.apiConnect(
+  process.env.MAILJET_API_KEY,
+  process.env.MAILJET_SECRET_KEY
+);
 
+async function sendMailjetEmail(toEmail, toName, message) {
+  try {
+    const request = mailjet.post("send", { version: "v3.1" }).request({
+      Messages: [
+        {
+          From: {
+            Email: "izzidinsamara@gmail.com",
+            Name: "TeamBase",
+          },
+          To: [
+            {
+              Email: toEmail,
+              Name: toName,
+            },
+          ],
+          Subject: "Thanks for contacting us!",
+          TextPart: `Hi ${toName},\n\nThis is TeamBase support Team, \n\nThank you for your message. We will get back to you shortly.\n\nTeamBase`,
+        },
+      ],
+    });
+
+    const result = await request;
+    console.log(result.body);
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 module.exports.addContact = async (req, res) => {
   try {
@@ -12,6 +45,7 @@ module.exports.addContact = async (req, res) => {
       message,
     });
 
+    await sendMailjetEmail(email, name, message);
     res.status(201).json(contact);
   } catch (error) {
     res.status(400).json({ error: "Failed to Add contact" });
