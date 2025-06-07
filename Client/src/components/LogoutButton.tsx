128a385c9307dbad3a6db267f5206b1f5ee6b2e0
Izzsamara <izzidinsamara@gmail.com>
2025-06-07 18:39:27 +0300

    Update text size, width, and add margin bottom to LogoutButton

--- Client/src/components/LogoutButton.tsx
+++ Client/src/components/LogoutButton.tsx
@@ -34,7 +34,7 @@ const LogoutButton: React.FC<LogoutButtonProps> = () => {
   return (
     <button
       onClick={handleLogout}
-      className="flex items-center gap-2 bg-gray-500 justify-center font-bold hover:bg-gray-600 mt-8 text-xl p-3 w-40 text-white rounded-lg focus:ring-5 focus:ring-gray-300 cursor-pointer"
+      className="flex items-center gap-2 bg-gray-500 justify-center font-bold hover:bg-gray-600 mt-8 text-md p-3 w-24 text-white rounded-lg focus:ring-5 focus:ring-gray-300 cursor-pointer mb-8"
       disabled={loading}
     >
       {loading ? "Logging out..." : "Logout"}
