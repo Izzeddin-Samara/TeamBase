0423968980551e5437764d343df79f8cf77ff6c6
Izzsamara <izzidinsamara@gmail.com>
2025-06-08 10:00:56 +0300

    Add padding and increase height in Navbar component

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -18,8 +18,8 @@ const Navbar = () => {
   if (!authChecked) return null;
 
   return (
-    <nav className="w-full fixed bg-blue-100 p-4">
-      <div className="max-w-[95%] mx-auto flex justify-between items-center h-10">
+    <nav className="w-full fixed bg-blue-100 ">
+      <div className="max-w-[95%] mx-auto flex justify-between items-center h-20">
         <Link to={isLoggedIn ? "/dashboard" : "/"}>
           <h1 className="text-3xl md:text-4xl font-bold cursor-pointer">
             Team<span className="text-blue-800">Base</span>
